bf2e14051169f1c80877396ec21b559c859546c5
Kevin <kevin.morris@nyu.edu>
2020-06-09 15:00:45 -0400

    updates

--- code/07_regs.R
+++ code/07_regs.R
@@ -1,11 +1,38 @@
-reg_data <- readRDS("./temp/reg_data.rds")
-roll <- readRDS("./temp/match_data.rds")
+reg_data <- readRDS("./temp/reg_data_no_age.rds")
+
+roll <- readRDS("./temp/match_data.rds") %>% 
+  select(-age)
 
 roll <- roll[complete.cases(roll), ] %>% 
   mutate(id = row_number())
 
+#######################
+cities <- readRDS("./temp/mke_voters.rds") %>% 
+  select(LALVOTERID, Residence_Addresses_City, County)
+cities <- cities[!duplicated(cities$LALVOTERID), ]
+
+roll <- left_join(roll, cities)
+rm(cities)
 ###########################
-ll <- reg_data %>% 
+
+reg_data <- left_join(reg_data, roll, by = "id")
+
+reg_data$group <- as.character(reg_data$group)
+
+inter <- as.data.table(filter(reg_data, distance <= 0.5))
+
+inter <- inter[ , count := .N, by = .(group, mke)] %>% 
+  mutate(weights = 1 / count)
+
+inter <- as.data.table(inter)[, .(weights = sum(weights)),
+                              by = list(id, group, County, primary_20,
+                                        primary_18, primary_16, white, black,
+                                        latino, asian, income, college, dem,
+                                        rep, male)] %>% 
+  mutate(mke = id == group)
+
+
+ll <- inter %>% 
   group_by(mke) %>% 
   summarize_at(vars(starts_with("primary_")), ~ weighted.mean(., weights))
 
@@ -16,56 +43,68 @@ ggplot(ll, aes(x = year, y = to, linetype = mke)) + geom_line()
 
 #############################
 
-m1 <- lm(primary_20 ~ mke, reg_data, weights = weights)
+m1 <- lm(primary_20 ~ mke + County, inter, weights = weights)
 
 m2 <- lm(primary_20 ~ mke +
-           age + primary_18 + primary_16 + white + black +
+           primary_18 + primary_16 + white + black +
            latino + asian + income + college + dem +
-           rep + male, reg_data, weights = weights)
+           rep + male + County, inter, weights = weights)
 
-m3 <- lm(primary_20 ~ mke * black, data = reg_data, weights = weights)
+m3 <- lm(primary_20 ~ mke * black + County, data = inter, weights = weights)
 
 m4 <- lm(primary_20 ~ mke * black +
-           age + primary_18 + primary_16 + white + 
+           primary_18 + primary_16 + white + 
            latino + asian + income + college + dem +
-           rep + male, reg_data, weights = weights)
+           rep + male + County, inter, weights = weights)
 
 stargazer(m1, m2, m3, m4,
           header = F,
           type = "text", notes.align = "l",
           covariate.labels = c("Lives in MKE", "Black", "Black × Lives in MKE"),
           dep.var.labels = c("Turnout"),
-          title = "\\label{tab:trip-diff} Turnout, 2010 --- 2018",
+          title = "\\label{tab:reg-table} Turnout in 2020 Primary",
           table.placement = "H",
           omit.stat = c("f", "ser", "aic"),
           omit = c("age", "primary_18", "primary_16", "white",
                    "latino", "asian", "income", "college", "dem",
-                   "rep", "male"),
+                   "rep", "male", "County"),
           table.layout = "-cmd#-t-a-s-n",
-          out = "./temp/test.tex",
+          out = "./temp/ccc.tex",
           out.header = F,
           notes = "TO REPLACE",
-          se = list(coef(summary(m1, cluster = c("group_id")))[, 2],
+          se = list(coef(summary(m1, cluster = c("group")))[, 2],
                     coef(summary(m2, cluster = c("group")))[, 2],
                     coef(summary(m3, cluster = c("group")))[, 2],
                     coef(summary(m3, cluster = c("group")))[, 2]),
-          add.lines=list(c("Includes Other Matched Covariates" , "", "X", "", "X")))
+          add.lines=list(c("Includes Other Matched Covariates" , "", "X", "", "X"),
+                         c("Includes County Fixed Effects" , "X", "X", "X", "X")))
 ###############
-
-reg_data <- left_join(reg_data, roll %>% 
-                        select(id, black), by = c("group" = "id"))
-
-cints <- rbindlist(lapply(c(1:40), function(m){
-  m <- m / 4
+cints <- rbindlist(lapply(c((1 / 35.2),
+                            seq(0.25, 2, 0.25),
+                            seq(2.5, 5, 0.5),
+                            c(6:10)), function(m){
+  print(m)
   
-  inter <- filter(reg_data, distance <= m) %>% 
-    group_by(group) %>% 
-    mutate(weights = ifelse((sum(weights) == 1) | mke, weights,
-                            1 / (n() - 1)))
+  inter <- as.data.table(filter(reg_data, distance <= m))
   
-  mod <- lm(primary_20 ~ mke * black.y, inter, weights = weights)
+  inter <- inter[ , count := .N, by = .(group, mke)] %>% 
+    mutate(weights = 1 / count)
+  
+  inter <- as.data.table(inter)[, .(weights = sum(weights),
+                                    primary_20 = mean(primary_20),
+                                    black = mean(black)), by = list(id, group, County)] %>% 
+    mutate(mke = id == group)
+  
+  if(length(unique(inter$County)) > 1){
+    mod <- lm(primary_20 ~ mke * black + County, inter, weights = weights)
+  } else{
+    mod <- lm(primary_20 ~ mke * black, inter, weights = weights)
+  }
+  
   
   ci <- confint(mod)
+  
+  nr <- nrow(ci)
   
   j <- data.table(distance = m,
                   estimate = mod[["coefficients"]][["mkeTRUE"]],
@@ -74,24 +113,41 @@ cints <- rbindlist(lapply(c(1:40), function(m){
                   type = "overall")
   
   k <- data.table(distance = m,
-                  estimate = mod[["coefficients"]][["mkeTRUE:black.yTRUE"]],
-                  lower = ci[4,1],
-                  upper = ci[4,2],
+                  estimate = mod[["coefficients"]][["mkeTRUE:black"]],
+                  lower = ci[nr,1],
+                  upper = ci[nr,2],
                   type = "black_add")
   
   return(bind_rows(j, k))
 }))
 
-cints <- mutate(cints, distance_jig = ifelse(type == "black_add", distance + 0.05, distance))
+saveRDS(cints, "./temp/cints_no_age_interaction.rds")
 
-ggplot(cints) +
-  geom_errorbar(aes(x = distance_jig,
+##############################################
+cints <- readRDS("./temp/cints_no_age_interaction.rds") %>% 
+  mutate(type = ifelse(type == "overall", "Lives in MKE",
+                       "Black × Lives in MKE"))
+
+cints$type <- relevel(as.factor(cints$type), ref = "Lives in MKE")
+
+plot <- ggplot(filter(cints)) +
+  geom_errorbar(aes(x = distance,
                     ymin = lower, ymax = upper, color = type), width = .2) +
-  geom_line(aes(x = distance_jig, y = estimate, color = type)) + 
-  geom_point(aes(x = distance_jig, y = estimate, color = type)) + 
-  geom_hline(yintercept = 0) +
+  geom_line(aes(x = distance, y = estimate, linetype = type, color = type)) + 
+  geom_point(aes(x = distance, y = estimate, color = type, shape = type), size = 3) + 
   theme_bw() +
-  labs(y = "Estimated Effect of Living in Milwaukee",
+  labs(y = "Estimated Coefficient",
        x = "Maximum Distance Between Control and Treated Voters (Miles)",
-       caption = "95% confidence bands shown.") +
-  scale_y_continuous(labels = scales::percent)
+       caption = "Notes: 95% confidence bars shown.",
+       color = "Variable",
+       shape = "Variable",
+       linetype = "Variable") +
+  scale_y_continuous(labels = scales::percent, breaks = seq(-.15, .1, 0.05)) +
+  scale_x_continuous(breaks = seq(0, 12, 2)) +
+  theme(text = element_text(family = "LM Roman 10"),
+        plot.caption = element_text(hjust = 0)) +
+  scale_color_manual(values = c("gray", "black")) +
+  geom_hline(yintercept = 0, linetype = "dashed")
+
+plot
+saveRDS(plot, "./temp/coef_plot.rds")
